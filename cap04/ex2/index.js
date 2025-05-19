81472045690897678e586bc2662333d60ab0802c
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-05-19 12:16:56 -0300

    adicionado exercicio 03 do capitulo 4

--- cap04/ex2/index.js
+++ cap04/ex2/index.js
@@ -8,12 +8,20 @@ form.addEventListener('submit', (e) => {
     const masculino = form.inMasculino.checked;
     const altura = Number(form.inAltura.value);
 
-    let peso;
+
+    const peso = masculino ? 22 * Math.pow(altura, 2) : 21 * Math.pow(altura, 2);
+
+    //Opção com if e else
+    /*let peso;
     if(masculino){
         peso = 22 * Math.pow(altura, 2)
     }else {
         peso = 21 * Math.pow(altura, 2)
-    }
+    } */
 
     resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} Kg`;
+})
+
+form.addEventListener('reset', () => {
+    resp.innerText = "";
 })
