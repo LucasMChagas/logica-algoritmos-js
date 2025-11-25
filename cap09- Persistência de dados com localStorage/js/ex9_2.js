8b88fd04c5228304fe18c9d97d16eb9ded6751b2
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-11-24 22:57:12 -0300

    adicionado exercício 02 do capitulo 9

---
+++ cap09- Persistência de dados com localStorage/js/ex9_2.js
@@ -0,0 +1,51 @@
+const inRadios = document.querySelectorAll('input');
+const imgClube = document.querySelector('#imgClube');
+const dvTitulo = document.querySelector('#divTitulo');
+
+const trocarClube = ()=> {
+    const clubes = ["Brasil", "Pelotas", "Farroupilha"];
+
+    let selecao;
+    for (let i = 0; i < inRadios.length; i++) {
+        if (inRadios[i].checked) {
+            selecao = i;
+            break
+        }
+    }
+
+    if (selecao <= 2){
+        dvTitulo.className = `row cores-${clubes[selecao]}`;
+
+        imgClube.src = `img/${clubes[selecao].toLowerCase()}.png`;
+        imgClube.className = "img-fluid";
+        imgClube.alt = `Simbolo do ${clubes[selecao]}`;
+
+        localStorage.setItem("clube", clubes[selecao]);
+    }else{
+        dvTitulo.className = "row";
+        imgClube.className = "d-none";
+        imgClube.alt = "";
+        localStorage.removeItem("clube");
+    }
+}
+
+const verificarClube = ()=> {
+    if (localStorage.getItem("clube")) {
+        const clube = localStorage.getItem("clube");
+
+        if (clube === "Brasil"){
+            inRadios[0].checked = true;
+        }else if(clube === "Pelotas"){
+            inRadios[1].checked = true;
+        }else{
+            inRadios[2].checked = true;
+        }
+        trocarClube();
+    }
+}
+
+window.addEventListener("load", verificarClube);
+
+for(const inRadio of inRadios) {
+    inRadio.addEventListener("change", trocarClube)
+}
